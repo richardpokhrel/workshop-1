ede6b8940aadaf74633960de994c51259ec2ee3f
richardpokhrel <108170976+richardpokhrel@users.noreply.github.com>
2022-10-03 04:24:28 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+      colors: {
+        white:"#ffffff",
+    
+        
+        gray:{
+          50:"#e7e7e7",
+          100:"#cfcfcf",
+          200:"#b8b8b8",
+          300:"#a0a0a0",
+          400:"#888888",
+          500:"#707070",
+          600:"#585858",
+          700:"#414141",
+          800:"#292929",
+          900:"#111111",
+        },
+        yellow:{
+         50:"#fef6e6",
+         100:"#feecce",
+         200:"#fbe3b5",
+         300:"#fdd99c",
+         400:"#fcdo84",
+         500:"#fbc76b",
+         600:"#fbbd52",
+         700:"#fab439",
+         800:"#faaa21",
+         900:"#f9a1o8",
+        },
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
